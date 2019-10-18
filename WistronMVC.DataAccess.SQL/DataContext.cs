916978200ddd5e620655d400a5b6d9d124566acb
JackRabbit <roycewnag@gmail.com>
2019-10-18 17:07:40 +0800

    finished EnityFramework install SQLrepostory

---
+++ WistronMVC.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using WIstronMVC.Core.Models;
+
+namespace WistronMVC.DataAccess.SQL
+{
+    public class DataContext: DbContext
+    {
+        public DataContext() :
+            base("DefaultConnection")
+        { }
+
+        public DbSet<User> User { get; set; }
+    }
+}
---
+++ WistronMVC.DataAccess.SQL/SQLRepostory.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using WistronMVC.Core.Models;
+using WIstronMVC.Core.Contracts;
+
+namespace WistronMVC.DataAccess.SQL
+{
+    public class SQLRepostory<T> : IRepostory<T> where T : BaseUser
+    {
+        DataContext context;
+        DbSet<T> Dbset;
+        public SQLRepostory(DataContext context)
+        {
+            this.context = context;
+            Dbset = context.Set<T>();
+        }
+        public IQueryable<T> Collection()
+        {
+            return Dbset;
+        }
+
+        public void Commit()
+        {
+            context.SaveChanges();
+        }
+
+        public void Delete(string Id)
+        {
+            var t = Find(Id);
+            if (context.Entry(t).State == EntityState.Detached)
+                Dbset.Attach(t);
+            Dbset.Remove(t);
+        }
+
+        public T Find(string Id)
+        {
+            return Dbset.Find(Id);
+        }
+
+        public void Insert(T t)
+        {
+            Dbset.Add(t);
+        }
+
+        public void Update(T t)
+        {
+            Dbset.Attach(t);
+            context.Entry(t).State = EntityState.Modified;
+        }
+    }
+}
